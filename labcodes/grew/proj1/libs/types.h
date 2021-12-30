1a84b9b2ba608f05370a9fbc04074200a73d9f0a
mingshashan <aikongfu>
2021-12-29 23:42:42 -0800

    update types

--- labcodes/grew/proj1/libs/types.h
+++ labcodes/grew/proj1/libs/types.h
@@ -13,7 +13,7 @@ typedef int bool;
 typedef char int8_t;
 typedef unsigned char uint8_t;
 typedef short int16_t;
-typedef unsigned char uint16_t;
+typedef unsigned short uint16_t;
 typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef long long int64_t;
