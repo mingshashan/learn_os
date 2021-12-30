e55977cc7e6e3eedc64fc060c42cbc9a4eb5f1ae
aikongfu <aikongfu@163.com>
2021-12-30 15:26:42 +0800

    reset

--- labcodes/grew/proj1/libs/x86.h
+++ labcodes/grew/proj1/libs/x86.h
@@ -8,17 +8,19 @@
 static inline uint8_t inb(uint16_t port) __attribute__((always_inline));
 static inline void outb(uint16_t port, uint8_t data) __attribute__((always_inline));
 
-static inline uint8_t
-inb(uint16_t port) {
+static inline uint8_t inb(uint16_t port) {
     uint8_t data;
-    asm volatile ("inb %1, %0" : "=a" (data) : "d" (port) : "memory");
+    // asm volatile ("inb %1, %0": "=a" (data): "d" (port) : "memory");
+    // asm volatile ("inb %1, %0": "=a" (data): "d" (port));
+    asm volatile ("inb %1, %0" : "=a" (data) : "d" (port));
     return data;
 }
 
-static inline void
-outb(uint16_t port, uint8_t data) {
-    asm volatile ("outb %0, %1" :: "a" (data), "d" (port) : "memory");
+
+static inline void outb(uint16_t port, uint8_t data) {
+    // asm volatile ("outb %0, %1" :: "a" (data), "d" (port) : "memory");
+    // asm volatile ("outb %0, %1" :: "a" (data), "d" (port) );
+    asm volatile ("outb %0, %1" :: "a" (data), "d" (port));
 }
 
-#endif /* !__LIBS_X86_H__ */
-
+#endif /* __LIBS_X86_H__ */
--- labcodes/grew/proj1/tools/sign.c
+++ labcodes/grew/proj1/tools/sign.c
@@ -3,23 +3,27 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     struct stat st;
-    if (argc != 3) {
-        fprintf(stderr, "Usage: <input filename> <output filename>\n");
+    
+    if (3 != argc) {
+        fprintf(stderr, "Usage: <input filename> <output filename>");
         return -1;
     }
-    if (stat(argv[1], &st) != 0) {
+
+    if (0 != stat(argv[1], &st)) {
         fprintf(stderr, "Error opening file '%s': %s\n", argv[1], strerror(errno));
         return -1;
     }
+
     printf("'%s' size: %lld bytes\n", argv[1], (long long)st.st_size);
     if (st.st_size > 510) {
         fprintf(stderr, "%lld >> 510!!\n", (long long)st.st_size);
         return -1;
     }
+
     char buf[512];
+    // 把buf内容都设置为0
     memset(buf, 0, sizeof(buf));
     FILE *ifp = fopen(argv[1], "rb");
     int size = fread(buf, 1, st.st_size, ifp);
@@ -27,17 +31,20 @@ main(int argc, char *argv[]) {
         fprintf(stderr, "read '%s' error, size is %d.\n", argv[1], size);
         return -1;
     }
+
     fclose(ifp);
     buf[510] = 0x55;
     buf[511] = 0xAA;
+    
     FILE *ofp = fopen(argv[2], "wb+");
     size = fwrite(buf, 1, 512, ofp);
-    if (size != 512) {
+    
+    if (512 != size) {
         fprintf(stderr, "write '%s' error, size is %d.\n", argv[2], size);
         return -1;
     }
-    fclose(ofp);
+
+    fclose(ofp);   
     printf("build 512 bytes boot sector: '%s' success!\n", argv[2]);
     return 0;
 }
-
