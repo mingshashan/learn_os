7d08f6e96e923c61f0d2db858d8ba3ab0e748f86
mingshashan <aikongfu>
2021-12-29 23:01:18 -0800

    update asm

--- labcodes/grew/proj1/boot/asm.h
+++ labcodes/grew/proj1/boot/asm.h
@@ -8,14 +8,14 @@
 /* define a null asm segment */
 #define SEG_NULLASM         \
     .word 0, 0;             \
-    .byte 0, 0, 0, 0        \
+    .byte 0, 0, 0, 0        
 
 #define SEG_ASM(type, base, lim)                            \
-    .word(((lim) >> 12 & 0xffff), ((base) & 0ffff));        \
-    .byte(((base) >> 16) & 0xfff),                          \
-        (0x90|(type)),                                      \
+    .word(((lim) >> 12 & 0xffff), ((base) & 0xffff));       \
+    .byte(((base) >> 16) & 0xff),                          \
+        (0x90 | (type)),                                    \
         (0xc0 | (((lim) >> 28) & 0xf)),                     \
-        (((base) >> 24) & 0xff)                                                  \
+        (((base) >> 24) & 0xff)                             
 
 
 /* Application segment type bits */
